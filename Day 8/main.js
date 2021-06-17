70aed07890e96c51d0d7b1d75376a9672892dd57
Himanshu Agarwal <everythinghimanshu@gmail.com>
2021-06-18 00:51:42 +0530

    made pixel drawing

---
+++ Day 8/main.js
@@ -0,0 +1,9 @@
+
+let squares = document.getElementsByClassName('baksa');
+
+Array.prototype.forEach.call(squares, (el)=>{
+el.addEventListener('keypress', (event)=>{
+
+    el.classList.toggle('active');})
+});
+

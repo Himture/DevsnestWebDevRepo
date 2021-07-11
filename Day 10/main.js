dcf1a77c7dfc3a486c13e17b74bb6f61778ee64b
Himanshu Agarwal <everythinghimanshu@gmail.com>
2021-07-11 17:24:31 +0530

    Made major change

--- Day 10/main.js
+++
@@ -1,7 +0,0 @@
-
-let squares = document.getElementsByClassName('baksa');
-
-Array.prototype.forEach.call(squares, (el)=>{
-el.addEventListener('contextmenu', (event)=>{
-    el.classList.toggle('active');})
-});
